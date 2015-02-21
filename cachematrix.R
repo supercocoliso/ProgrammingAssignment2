b3658c666fb77c4cf1b0ed97167225999fdf8c14
supercocoliso <leandroide07@yahoo.es>
2015-02-21 20:32:24 +0100

    Final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to invert a matrix. The inverse is cached instead of compute it repeatedly.
 
-## Write a short comment describing this function
+
+## Builder for matrix object. It is actually a list of functions:
+##  set: sets the matrix
+##  get: gets the matrix
+##  setinverse: sets the inverted matrix
+##  getinverse: gets the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
@@ -19,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function accepts a matrix object and returns the inverted matrix
+## It first tests if the inverted matrix is present in the object.
+##  If present, it does not calculate it again.
+##  If absent, it calculates the inverted matrix and it stores it in the matrix object
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -34,6 +41,15 @@ cacheSolve <- function(x, ...) {
   inv
 }
 
-data <- makeCacheMatrix(matrix(runif(16),nrow=4))
-cacheSolve(data)
-cacheSolve(data)
+
+## The following code contains and example of how to use this functions:
+# data <- makeCacheMatrix(matrix(runif(16),nrow=4))
+# cat("\nMatrix:\n")
+# print(data$get())
+# cat("\nInverting matrix...\n")
+# cacheSolve(data)
+# cat("\nInverting matrix again...\n")
+# cacheSolve(data)
+# cat("\nInverted matrix:\n")
+# print(data$getinverse())
+
